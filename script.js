68d29d6c1b3dfab68a080ed80b79251ff1145594
styles76 <shane.sawler@mdoc.education>
2025-02-15 19:03:01 -0500

    add a j.s back to top and darkmode option

---
+++ script.js
@@ -0,0 +1,22 @@
+// get user selected text
+const getUserSelectedText = () => {
+    return window.getSelection().toString();
+}
+// knowing when user gets to bottom of page
+
+ window.onscroll = () => {
+    if((window.innerHeight + window.pageYOffset) >= document.body.offsetHeight) {
+        console.log('you are at the end of the page.')
+    }
+ }
+
+ //taking user back to top
+ const toTop = () => window.scrollTo({top: 0, behavior: 'smooth'})
+
+ // darkmode detection
+
+ const isDarkMode = () => {
+    return window.matchMedia('prefers-color-scheme: dark)').matches;
+ }
+
+ 
